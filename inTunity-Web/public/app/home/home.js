85799a1cb727bd730dd11d041e6525ddc99f4575
YenHuang <yenhuang@bellsouth.net>
2015-12-23 18:07:40 -0500

    changed all the urls

--- inTunity-Web/public/app/home/home.js
+++ inTunity-Web/public/app/home/home.js
@@ -52,7 +52,7 @@ angular.module( 'inTunity.home', [
 
 
     $http({
-      url: 'http://localhost:3001/secured/specificUser' ,
+      url: 'http://ec2-52-35-92-198.us-west-2.compute.amazonaws.com:3001/secured/specificUser' ,
       method: 'GET',
       params: {id: ppl_id}
     }).then(function(response) {  
@@ -105,7 +105,7 @@ angular.module( 'inTunity.home', [
   $scope.users;
 
   $http({
-    url: 'http://localhost:3001/secured/accounts' ,
+    url: 'http://ec2-52-35-92-198.us-west-2.compute.amazonaws.com:3001/secured/accounts' ,
     method: 'GET'
   }).then(function(response) {  
     songdata = (response["data"]["songs"]);
--- inTunity-Web/public/app/login/login.js
+++ inTunity-Web/public/app/login/login.js
@@ -66,7 +66,7 @@ angular.module( 'inTunity.login', [
       $location.path("/");
 
       console.log("posting..");
-      $http.post('http://localhost:3001/secured/account', {data: user_account}, { 
+      $http.post('http://ec2-52-35-92-198.us-west-2.compute.amazonaws.com:3001/secured/account', {data: user_account}, { 
           headers: {
           'Accept' : '*/*',
           'Content-Type': 'application/json'
