7453ed3c9aee80ce725ad7335b1f4d2e2f92578f
YenHuang <yenhuang@bellsouth.net>
2016-01-06 21:37:23 -0500

    gonna try to look into angular env

--- inTunity-Web/public/partials/addSong/addSong.js
+++ inTunity-Web/public/partials/addSong/addSong.js
@@ -8,6 +8,8 @@ angular.module( 'inTunity.addSong', [
   var globalPlayer;
 
 
+
+
   var prof = (store.get('profile'));
   $scope.owner;
   if (prof["given_name"] != null) {
@@ -401,7 +403,7 @@ angular.module( 'inTunity.addSong', [
               locationFlag: true
             });
 
-            console.log(song);
+   
 
 
             $http.post('http://localhost:3001/secured/account/id/song', {data: song}, { 
