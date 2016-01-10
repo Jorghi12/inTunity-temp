80c4c7749bd90d3355789df42acccbfe0eb6d802
Jorghi12 <jd728@cornell.edu>
2016-01-09 19:18:15 -0600

    Fixed Auto Start Bug

--- inTunity-Web/public/partials/stream/stream.js
+++ inTunity-Web/public/partials/stream/stream.js
@@ -626,7 +626,9 @@ app.controller('StreamCtrl', function StreamController($scope, auth, $http, $loc
 				userDisplay = ($scope.correctUsers[song_count]["user"][0]["nickname"] != null) ? $scope.correctUsers[song_count]["user"][0]["nickname"] : $scope.correctUsers[song_count]["user"][0]["given_name"];
 			
                 //Updates the current state of the player footer GUI
-                $scope.updateCurrentPlayerGraphics(songDuration);
+				if (window.globalPlayer != null){
+					$scope.updateCurrentPlayerGraphics(songDuration);
+				}
 				
 				$scope.setGraphics(userDisplay,artworkUrl,myTitle,songDuration);
 			} else {
