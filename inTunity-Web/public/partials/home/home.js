c7d44825c2a815f2dcdcbe88a78019dcc753b966
Jorghi12 <jd728@cornell.edu>
2016-02-09 20:25:13 -0500

    Update Right Now

--- inTunity-Web/public/partials/home/home.js
+++ inTunity-Web/public/partials/home/home.js
@@ -44,7 +44,7 @@ app.controller('HomeCtrl', function HomeController($scope, auth, $http, $locatio
     $scope.profile = function() {
         console.log("test");
         $http({
-            url: 'http://localhost:3001/secured/account/id',
+            url: 'http://ec2-52-33-107-31.us-west-2.compute.amazonaws.com:3001/secured/account/id',
             method: 'GET',
             params: {
                 id: id
@@ -74,7 +74,7 @@ app.controller('HomeCtrl', function HomeController($scope, auth, $http, $locatio
             song_id: song_id, 
             liked_user_id: id
         });
-        $http.post('http://localhost:3001/secured/account/id/likes/song/id', {data: likes}, { 
+        $http.post('http://ec2-52-33-107-31.us-west-2.compute.amazonaws.com:3001/secured/account/id/likes/song/id', {data: likes}, { 
               headers: {
               'Accept' : '*/*',
               'Content-Type': 'application/json'
@@ -113,7 +113,7 @@ app.controller('HomeCtrl', function HomeController($scope, auth, $http, $locatio
 		
 		//Grab a list of all the users
 		$http({
-         url: 'http://localhost:3001/secured/account/id/search',
+         url: 'http://ec2-52-33-107-31.us-west-2.compute.amazonaws.com:3001/secured/account/id/search',
         method: 'GET',
 		params: {searchString: $scope.searchUsers, userID: myUserId}
 		}).then(function(response) {
@@ -179,7 +179,7 @@ app.controller('HomeCtrl', function HomeController($scope, auth, $http, $locatio
 			user_id: myUserId,
             other_id: follower_user_id
         });
-        $http.post('http://localhost:3001/secured/account/id/addfollower', {data: followerData}, { 
+        $http.post('http://ec2-52-33-107-31.us-west-2.compute.amazonaws.com:3001/secured/account/id/addfollower', {data: followerData}, { 
               headers: {
               'Accept' : '*/*',
               'Content-Type': 'application/json'
@@ -200,7 +200,7 @@ app.controller('HomeCtrl', function HomeController($scope, auth, $http, $locatio
 
     $scope.getUserInfo = function() {
         $http({
-            url: 'http://localhost:3001/secured/account',
+            url: 'http://ec2-52-33-107-31.us-west-2.compute.amazonaws.com:3001/secured/account',
             method: 'GET'
         }).then(function(response) {
             var data = (response['data']['songs']);
@@ -227,7 +227,7 @@ app.controller('HomeCtrl', function HomeController($scope, auth, $http, $locatio
      
     $scope.getAllLocations = function() {
         $http({
-            url: 'http://localhost:3001/secured/location' ,
+            url: 'http://ec2-52-33-107-31.us-west-2.compute.amazonaws.com:3001/secured/location' ,
             method: 'GET'
         }).then(function(response) {  
 			//Remove Duplicates
