3c2b26c677ca0b0770cdea3df8a251610bde2463
Jorghi12 <jd728@cornell.edu>
2016-02-11 23:44:23 -0500

    GAINS

--- inTunity-Db/model/User.js
+++ inTunity-Db/model/User.js
@@ -13,10 +13,9 @@ var User = new Schema({
 	today_song: [ObjectId],
 	saved_song: [ObjectId],
 	friends: [ObjectId],
-	followers: [type: ObjectId, default:[]],
-	following: [type: ObjectId, default:[]]
+	followers: {type: Array,'default': []},
+	following: {type: Array,'default': []}
 });
-
 
 
 module.exports = mongoose.model('User', User);
