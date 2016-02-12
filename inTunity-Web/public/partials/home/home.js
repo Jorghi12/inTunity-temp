0a51d97baafe89eb0d3b2e105902e99497c2c3b6
YenHuang <yenhuang@bellsouth.net>
2016-02-12 14:57:44 -0500

    commented out

--- inTunity-Web/public/partials/home/home.js
+++ inTunity-Web/public/partials/home/home.js
@@ -495,21 +495,21 @@ app.controller('HomeCtrl', function HomeController($scope, auth, $http, $locatio
 
 
 
-       $(function() {
-            var offset = $("#sidebar").offset();
-            var topPadding = 0;
-            $(window).scroll(function() {
-                if ($(window).scrollTop() > offset.top) {
-                    $("#sidebar").stop().animate({
-                        marginTop: $(window).scrollTop() - offset.top + topPadding
-                    });
-                } else {
-                    $("#sidebar").stop().animate({
-                        marginTop: 0
-                    });
-                };
-            });
-        });
+       // $(function() {
+       //      var offset = $("#sidebar").offset();
+       //      var topPadding = 0;
+       //      $(window).scroll(function() {
+       //          if ($(window).scrollTop() > offset.top) {
+       //              $("#sidebar").stop().animate({
+       //                  marginTop: $(window).scrollTop() - offset.top + topPadding
+       //              });
+       //          } else {
+       //              $("#sidebar").stop().animate({
+       //                  marginTop: 0
+       //              });
+       //          };
+       //      });
+       //  });
 
 
 
