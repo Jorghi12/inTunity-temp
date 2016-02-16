7ab6f1848107e2c55343913372640ac74508c3ec
Jorghi12 <jd728@cornell.edu>
2016-02-16 00:40:22 -0500

    Updated API to include ReactJS

--- inTunity-API/api.js
+++ inTunity-API/api.js
@@ -16,7 +16,8 @@ new webpack.DefinePlugin({
     NODE_ENV: JSON.stringify("production")
   }
 });
-var authenticate = jwt({
+
+var authenticate = jwt({ 
   secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
   audience: process.env.AUTH0_CLIENT_ID
 });
