f34037418d847d9f3dfff4a1f9e3574e1fbc2643
Jorghi12 <jd728@cornell.edu>
2016-02-16 00:53:58 -0500

    Gains

--- inTunity-API/api.js
+++ inTunity-API/api.js
@@ -17,6 +17,11 @@ new webpack.DefinePlugin({
   }
 });
 
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('LEGEND')
+); 
+
 var authenticate = jwt({ 
   secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
   audience: process.env.AUTH0_CLIENT_ID
